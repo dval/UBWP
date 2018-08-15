98603fe8bddef38a4c26fae0ca6b4afe91bc0f6c
gamesolids <dylan.valentine@gmail.com>
2018-08-15 12:22:21 -0400

    overhaul KeyListener, removed static members, added gamepad

--- unity/UBWP/Assets/Scripts/UBWP Recorder/KeyListener.cs
+++ unity/UBWP/Assets/Scripts/UBWP Recorder/KeyListener.cs
@@ -1,82 +1,174 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEditor;
 
 /// <summary>
 /// A terrible mash of static instances....
 /// Uses a menu shortcut hack to listen for keydown in edit mode.
-/// uses normal event listener in play mode
-/// 
-/// Adds emnu item...
+/// uses normal event listener in play mode.
 /// </summary>
 
 namespace gamesolids
 {
     [ExecuteInEditMode]
-    public class KeyListener : MonoBehaviour {
-
-        //do you want 'p' in edit mode?
+    public class KeyListener : MonoBehaviour
+    {
         public bool ExecuteInEditMode = true;
+        
+        public enum Modifier { None = 0, CTRL = 1, SHIFT = 2, ALT = 3 }
+        [SerializeField]
+        Modifier ModifierKey = Modifier.ALT;
 
-        //Menu and KeyEvent both listen to 'P-Key'
         public string ListenToKey = "p";
-        [MenuItem("Tools/MapTool/InvokeShortcut _p")]
-        static void InvokeShortcut()
-        {
-            Instance.InvokeKeyEvent();
-        }
+
+        [Tooltip("The gamepad or controller button to listen to.")]
+        public string JoystickAxis = "Fire3";
         
         [Tooltip("Optional Button component to trigger when key is pressed.")]
-        public static Button ButtonToTrigger;
-        [Tooltip("Events to call directly when key is pressed.")]
-        public UnityEvent OnKeyDown;
-        
-        //Static Instance spaghetti management
-        [HideInInspector]
-        public static KeyListener Instance;
-        [HideInInspector]
-        public KeyListener instance;
+        public Button UIButton;
 
-        //Invoke the Button or Event to be triggered by key press.
+        [Tooltip("Events to call directly when key is pressed.")]
+        public UnityEvent OnKeyDown;// = new CustomKeyEvent();
+
+        /// <summary>
+        /// Constructer initializes the key mapped listeners
+        /// </summary>
+        public KeyListener() 
+        {
+            SceneView.onSceneGUIDelegate += view =>
+            {
+                Event e = Event.current;
+                if (e.type == EventType.KeyDown)
+                {
+                    //check if editmode is enabled
+                    if (this.ExecuteInEditMode)
+                    {
+                        RespondEvent(e);
+                    }
+                }
+            };
+        }
+        
+
+        /// <summary>
+        /// Invoke the Button or Event to be triggered by key press.
+        /// </summary>
         public void InvokeKeyEvent()
         {
-            if (KeyListener.ButtonToTrigger != null)
+            if (UIButton != null)
             {
-                this.GetComponent<Button>().onClick.Invoke();
+                UIButton.GetComponent<Button>().onClick.Invoke();
             }
 
             this.OnKeyDown.Invoke();
         }
-        
-        //Filter space key listener at start
+
+        /// <summary>
+        /// Filter space key listener at start
+        /// </summary>
         private void Start()
         {
             if (ListenToKey == " ") { ListenToKey = "space"; }
             
         }
-
-        //use update to listen for key state and trigger event
+        
+        /// <summary>
+        /// use update to listen for key state and trigger event during runtime
+        /// </summary>
         void Update()
         {
             //check if editmode is enabled
             if (this.ExecuteInEditMode || Application.isPlaying)
             {
-                if (Input.GetKeyDown(ListenToKey))
+                RespondEvent();
+            }
+        }
+
+        /// <summary>
+        /// Respond to combination of key and modifier during playmode
+        /// </summary>
+        public void RespondEvent()
+        {
+            if (Input.GetKeyDown(ListenToKey) || Input.GetButtonDown(JoystickAxis))
+            {
+                if (ModifierKey == Modifier.None || Input.GetButtonDown(JoystickAxis))
+                {
+                    this.InvokeKeyEvent();
+                }
+                else if (ModifierKey == Modifier.CTRL)
+                {
+                    if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl) || Input.GetButtonDown(JoystickAxis))
+                    {
+                        this.InvokeKeyEvent();
+                    }
+                }
+                else if (ModifierKey == Modifier.SHIFT)
+                {
+                    if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift) || Input.GetButtonDown(JoystickAxis))
+                    {
+                        this.InvokeKeyEvent();
+                    }
+                }
+                else if (ModifierKey == Modifier.ALT)
+                {
+                    if (Input.GetKeyDown(KeyCode.LeftAlt) || Input.GetKeyDown(KeyCode.RightAlt) || Input.GetButtonDown(JoystickAxis))
+                    {
+                        this.InvokeKeyEvent();
+                    }
+                }
+            }
+            //else a big fat nothing. Nope. Done.
+        }
+
+
+        /// <summary>
+        /// Respond to combination of key and modifier during edit mode
+        /// </summary>
+        bool ctrlFlag = false;
+        bool shftFlag = false;
+        bool altFlag = false;
+
+        public void RespondEvent(Event evt)
+        {
+            //Flag the modifier keys, since we can't listen to other key events from within this key event. (black hole?)
+            if (evt.type == EventType.KeyDown)
+            {
+                if (ModifierKey == Modifier.CTRL && (evt.keyCode == (KeyCode.LeftControl) || evt.keyCode == (KeyCode.RightControl)))
+                    ctrlFlag = true;
+                if (ModifierKey == Modifier.SHIFT && (evt.keyCode == (KeyCode.LeftShift) || evt.keyCode == (KeyCode.RightShift)))
+                    shftFlag = true;
+                if (ModifierKey == Modifier.ALT && (evt.keyCode == (KeyCode.LeftAlt) || evt.keyCode == (KeyCode.RightAlt)))
+                    altFlag = true;
+            }
+            if (evt.type == EventType.KeyUp)
+            {
+                if (ModifierKey == Modifier.CTRL && (evt.keyCode == (KeyCode.LeftControl) || evt.keyCode == (KeyCode.RightControl)))
+                    ctrlFlag = false;
+                if (ModifierKey == Modifier.SHIFT && (evt.keyCode == (KeyCode.LeftShift) || evt.keyCode == (KeyCode.RightShift)))
+                    shftFlag = false;
+                if (ModifierKey == Modifier.ALT && (evt.keyCode == (KeyCode.LeftAlt) || evt.keyCode == (KeyCode.RightAlt)))
+                    altFlag = false;
+            }
+
+            //Listen for lListenToKey
+            if (evt.keyCode == (KeyCode)System.Enum.Parse(typeof(KeyCode), ListenToKey.ToUpper()))
+            {
+                //check for modifiers and the like
+                if (ModifierKey == Modifier.None)
+                {
+                    this.InvokeKeyEvent();
+                }
+                else if (ctrlFlag || shftFlag || altFlag)
                 {
                     this.InvokeKeyEvent();
                 }
             }
-        }
-
-        //Ugly but working
-        //Fix static instance by twirling spaghetti
-        private void LateUpdate()
-        {
-            Instance = this;
-            this.instance = Instance;
+            //else nothing.
         }
     }
 }
